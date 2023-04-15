d88270e7b40e988c4c1385b47738fe4cdf58c80f
Geovane-note <geovane.ita@gmail.com>
2023-04-15 16:18:46 -0300

    feito classes

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import entities.OrderItem;
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product p = new Product("TV", 1000.00);
+
+        OrderItem oi1 = new OrderItem(1, 1000.0, p);
+
+        System.out.println(oi1);
+
+        sc.close();
+    }
+}
